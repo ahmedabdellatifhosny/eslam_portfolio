7a65ed9fa9f25551436fe6f72a7b4bb25039de6e
devahmed <ahmedabdellatifhosny@gmail.com>
2025-06-30 00:29:44 +0300

    update navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,15 +5,32 @@ export default function Navbar() {
       <div className="container">
         <div className=" flex justify-between align-center">
           <div className="logo">
-            <h1 className="text-3xl font-cairo font-bold text-teal-600">اسلام صبحي</h1>
+            <h1 className="text-3xl font-cairo font-bold text-teal-600">
+              اسلام صبحي
+            </h1>
           </div>
-          <div className="links font-cairo font-medium" >
-            <Link href="#" className="px-3">الصفحة الرئيسية</Link>
-            <Link href="#" className="px-3">من انا </Link>
-            <Link href="#" className="px-3">دوراتي</Link>
-            <Link href="#" className="px-3">خدماتي</Link>
-            <Link href="#" className="px-3">اعمالي</Link>
-            <Link href="#" className="px-3">تواصل معي</Link>
+          <div className="links font-cairo font-medium">
+            <Link
+              href="#"
+              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+            >
+              الصفحة الرئيسية
+            </Link>
+            <Link href="#" className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50">
+              من انا{" "}
+            </Link>
+            <Link href="#" className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50">
+              دوراتي
+            </Link>
+            <Link href="#" className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50">
+              خدماتي
+            </Link>
+            <Link href="#" className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50">
+              اعمالي
+            </Link>
+            <Link href="#" className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50">
+              تواصل معي
+            </Link>
           </div>
         </div>
       </div>
