7892f551572de26bb9d146e19a72d5871774c78f
devahmed <ahmedabdellatifhosny@gmail.com>
2025-07-04 02:02:22 +0300

    updaate style

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,9 +1,16 @@
 export default function Hero() {
   return (
     <div className="hero flex flex-col justify-center items-center">
-        <h3 className=" pt-10 font-cairo font-bold">مرحباً انا </h3>
-        <h1 className=" py-6 font-cairo font-bold uppercase  text-teal-600  "> eslam sobhey</h1>
-        <h4 className=" py-6 text-3xl font-cairo font-semibold text-gray-700">كاتب محتوى تسويقي</h4>
+      <h3 className=" pt-10 text-5xl font-cairo font-bold md:text-7xl">
+        مرحباً انا{" "}
+      </h3>
+      <h1 className=" py-6 text-5xl font-cairo font-bold text-teal-600 uppercase md:text-9xl">
+        {" "}
+        eslam sobhey
+      </h1>
+      <h4 className=" text-2xl font-cairo font-semibold text-gray-700 md:text-3xl ">
+        كاتب محتوى تسويقي
+      </h4>
     </div>
-  )
+  );  
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,48 +6,64 @@ export default function Navbar() {
         <div className=" flex justify-between align-center">
           <div className="logo">
             <h2 className="text-3xl font-cairo font-bold text-teal-600 uppercase">
-              eslam sobhey
+              <Link href="/">eslam sobhey</Link>
             </h2>
           </div>
-          <div className="links font-cairo">
-            <Link
-              href="#"
-              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
-            >
-              الصفحة الرئيسية
-            </Link>
-            <Link
-              href="#"
-              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
-            >
-              من انا 
-            </Link>
-
-            
-            <Link
-              href="#"
-              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
-            >
-              دوراتي
-            </Link>
-            <Link
-              href="#"
-              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
-            >
-              خدماتي
-            </Link>
-            <Link
-              href="#"
-              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
-            >
-              اعمالي
-            </Link>
-            <Link
-              href="#"
-              className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
-            >
-              تواصل معي
-            </Link>
+          <div className="links font-cairo hidden md:block">
+            <ul className="flex items-center">
+              <li>
+                <Link
+                  href="#"
+                  className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+                >
+                  الصفحة الرئيسية
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  href="#"
+                  className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+                >
+                  من انا
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  href="#"
+                  className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+                >
+                  دوراتي
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+                >
+                  خدماتي
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  href="#"
+                  className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+                >
+                  اعمالي
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link
+                  href="#"
+                  className="px-3  rounded-sm cursor-pointer transition-all hover:text-teal-600 hover:bg-teal-50"
+                >
+                  تواصل معي
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
